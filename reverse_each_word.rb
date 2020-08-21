ad46d6151065f42d43bb639205fc3f52415ee7ae
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-21 06:22:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,3 @@
-def reverse_each_word(sentence1)
-  words_reversed = []
-  original_sentence = sentence1.split
-    original_sentence.collect >> sentence1 {|text| text.reverse}
-  end
-  words_reversed.join(" ")
+def reverse_each_word(sentence)
+array = new_array.collect {|sentence)| setence.reverse}
 end
