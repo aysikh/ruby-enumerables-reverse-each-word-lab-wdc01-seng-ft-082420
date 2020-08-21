d5abae2efec7e1bb13e00a88164fb9fc0bfcfa78
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-21 06:26:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,4 @@
 def reverse_each_word(string)
-new_array = string.split
-array.collect {|x)| x.reverse}
-new_array << array.join(" ")
+array = string.split(" ")
+array.collect {|x)| x.reverse}.join
 end
